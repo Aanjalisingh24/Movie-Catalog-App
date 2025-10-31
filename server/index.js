c7ff57d7e153b72dcad10e4db51595d659f2527f
Aanjalisingh24 <aanjali23@navgurukul.org>
2025-10-31 18:05:11 +0530

    Added frontend live link to CORS

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'movie-catalog-frontend-pi.vercel.app', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
