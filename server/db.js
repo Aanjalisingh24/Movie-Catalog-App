6c23461b87ff887f512bd64073cbaa94461a5432
Aanjalisingh24 <aanjali23@navgurukul.org>
2025-10-31 18:42:17 +0530

    made changes in backend database

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,  // ✅ add this line
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306,  // ✅ add this line
 });
 
 module.exports = db;
