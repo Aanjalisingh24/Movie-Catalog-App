c385f5aff2b1636023f29bf3b25728373a3140e1
Aanjalisingh24 <aanjali23@navgurukul.org>
2025-10-31 18:08:55 +0530

    correct cors link

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 app.use(cors({
-  origin: 'movie-catalog-frontend-pi.vercel.app', 
+  origin: 'https://movie-catalog-frontend-pi.vercel.app', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
